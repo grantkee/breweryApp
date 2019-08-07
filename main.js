6d1e7f0a9c4755bc759b4d420a61cf626ddc7c9b
jaywiles <jay.wiles@gmail.com>
2019-08-06 20:56:08 -0500

    yep

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 let checkFetch = function(response) {
     if (!response.ok) {
@@ -8,26 +8,58 @@ let checkFetch = function(response) {
 }
 
 
-function getAPI(){
-    fetch('https://sandbox-api.brewerydb.com/v2/')
+const lat = position.coords.latitude;
+const lon = position.coords.longitude;
+
+
+
+// status.textContent = `Latitude: ${lat}, Longitude: ${lon}`;
+
+// /search/geo/point?lat=35.772096&lng=-78.638614
+
+const locationURL = () => {
+    
+}
+
+function getBeer(){
+    fetch('https://sandbox-api.brewerydb.com/v2/search/geo/point/?key=d2bcebda18ee4340157eff0fc2192936', {'mode': 'no-cors'})
     .then(checkFetch)
+    console.log("id is working")
     .then((result) => result.json())
     .then((data) => {
-        let output = `<h2>Friends</h2>`
-        data.results.forEach((function(user){
-            output += `
-            <img src = "${user.picture.medium}" alt = "faces">
-                <ul>
-                    <li>${user.name.first} ${user.name.last}</li>
-                    <li>${user.location.city}, ${user.location.state}</li>
-                    <li>${user.email}</li>
-                </ul>
-            `
-        }))
-        document.getElementById('container').innerHTML = output
+
+
+
+    
     })
     .catch(function(err){
         console.log('Error');
         console.log(err);
     })
 };
+getBeer();
+
+
+// function getAPI(){
+//     fetch('https://sandbox-api.brewerydb.com/v2/search/geo/point/?key=d2bcebda18ee4340157eff0fc2192936')
+//     .then(checkFetch)
+//     .then((result) => result.json())
+//     .then((data) => {
+//         let output = `<h2>Friends</h2>`
+//         data.results.forEach((function(user){
+//             output += `
+//             <img src = "${user.picture.medium}" alt = "faces">
+//                 <ul>
+//                     <li>${user.name.first} ${user.name.last}</li>
+//                     <li>${user.location.city}, ${user.location.state}</li>
+//                     <li>${user.email}</li>
+//                 </ul>
+//             `
+//         }))
+//         document.getElementById('container').innerHTML = output
+//     })
+//     .catch(function(err){
+//         console.log('Error');
+//         console.log(err);
+//     })
+// };
