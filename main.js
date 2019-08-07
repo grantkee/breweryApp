f04ff7e1db040ead3831299d4fcb575fc3eafb2c
grantkee <grant.d.kee@gmail.com>
2019-08-06 20:57:41 -0500

    getting server 400 error because we need lat and lon values

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 let checkFetch = function(response) {
     if (!response.ok) {
@@ -7,23 +7,27 @@ let checkFetch = function(response) {
     return response;
 }
 
+function getLocation() {
+    var x = document.getElementById("container");
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
 
-function getAPI(){
-    fetch('https://sandbox-api.brewerydb.com/v2/')
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude; 
+}
+
+
+function getBeer(){
+    fetch('https://sandbox-api.brewerydb.com/v2/search/geo/point/?key=d2bcebda18ee4340157eff0fc2192936', {'mode': 'no-cors'})
     .then(checkFetch)
     .then((result) => result.json())
     .then((data) => {
-        let output = `<h2>Friends</h2>`
-        data.results.forEach((function(user){
-            output += `
-            <img src = "${user.picture.medium}" alt = "faces">
-                <ul>
-                    <li>${user.name.first} ${user.name.last}</li>
-                    <li>${user.location.city}, ${user.location.state}</li>
-                    <li>${user.email}</li>
-                </ul>
-            `
-        }))
+
         document.getElementById('container').innerHTML = output
     })
     .catch(function(err){
