6a2ca4b15e428d23ceff2b7bfb37aea1df043a9c
jaywiles <jay.wiles@gmail.com>
2019-08-11 16:45:15 -0500

    progress

--- main.js
+++ main.js
@@ -1,65 +1,59 @@
 // require('dotenv').config();
 
-let checkFetch = function(response) {
-  if (!response.ok) {
-    throw Error(response.statusText + ' - ' + response.url);
-  }
-  return response;
-} 
+// let checkFetch = function(response) {
+//   if (!response.ok) {
+//     throw Error(response.statusText + ' - ' + response.url);
+//   }
+//   return response;
+// } 
 
-`https://sandbox-api.brewerydb.com/v2/search/geo/point?lat=${lat}&lng=${lng}`
+// `https://sandbox-api.brewerydb.com/v2/search/geo/point?lat=${lat}&lng=${lng}`
 
-let myLocation = document.getElementById("my-coordinates")
 
-function getLocation() {
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(displayPosition)
-  } else {
-    alert("Sorry, location is not available. Stay sober, friends.")
-  }
-}
-
-function displayPosition(position) {
-  const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  let loc = lat + ", " + lng;
-
-  myLocation.innerHTML = " Latitude: " + lat + ", " + "Longitude: " + lng;
-}
 
 function createMap() {
-    
-  var map = new ol.Map({
-    target: document.querySelector('map'),
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([lat, lng]),
-      zoom: 4
-    })
-  });
-    
-    // ! NEXT TWO LINES DEPEND ON GOOGLE MAPS API WORKING !
-    // let map = new google.maps.Map(document.getElementById(#map), {zoom: 4, center: loc});
-    // let homeMarker = new google.maps.Marker({position: loc, map: map})
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+  const mapImage = document.querySelector('#map-image');
+  mapLink.href = '';
+  mapLink.textContent = '';
+  function success(position) {
+      let lat = position.coords.latitude;
+      let lng = position.coords.longitude;
+      status.textContent = '';
+      mapImage.innerHTML =`
+      <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=${lng}%2C${lat}%2C${lng}%2C${lat}&amp;layer=mapnik" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=18/${lat}/${lng}">View Larger Map</a></small>
+      `
+      mapLink.innerHTML = `Latitude: ${lat} °, Longitude: ${lng} °`;
+      getBeer(position);
+  }
+  function error() {
+      status.innerHTML = 'Unable to retrieve your location';
+  }
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating...';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
 }
+document.querySelector('#find-user').addEventListener('click', createMap);
+// document.querySelector('#find-user').addEventListener('onload', getBeer);
 
-// createMap();
+// console.log(lat + ", " + lng)
 
-function getBeer(){
-    fetch('https://sandbox-api.brewerydb.com/v2/?key=d2bcebda18ee4340157eff0fc2192936', {'mode': 'no-cors'})
-    .then(checkFetch)
+
+function getBeer(position){
+  let lat = position.coords.latitude;
+  let lng = position.coords.longitude;
+  console.log(lat + ", " + lng);
+  fetch(`https://sandbox-api.brewerydb.com/v2/search/geo/point?lat=${lat}&lng=${lng}&totalResults&key=d2bcebda18ee4340157eff0fc2192936`)
+    // .then(checkFetch)
     // console.log("id is working")
-    .then((result) => result.json())
+    // .then((result) => result.json())
     .then((data) => {
-        // look at last line of code in dashboard.js -- calls button for getting location
-      // let output = `<h2>Beer Near Me!</h2>`
-      // data.results.forEach((function(user) {
-        // output += ``
-      // }))
+      // let output = `<h2>Results: ${totalResults}</h2>`;
+      console.log(data);
 
     
     })
@@ -68,11 +62,21 @@ function getBeer(){
     //     console.log(err);
     // })
 };
-getBeer();
+
+
+
+
+
+
+
+
+
+
+
 
 
 // function getAPI(){
-//     fetch('https://sandbox-api.brewerydb.com/v2/search/geo/point/?key=d2bcebda18ee4340157eff0fc2192936')
+//     fetch(`https://sandbox-api.brewerydb.com/v2/search/geo/point?lat=${lat}&lng=${lng}&key=d2bcebda18ee4340157eff0fc2192936`)
 //     .then(checkFetch)
 //     .then((result) => result.json())
 //     .then((data) => {
@@ -94,3 +98,70 @@ getBeer();
 //         console.log(err);
 //     })
 // };
+
+// let myLocation = document.getElementById("my-coordinates")
+
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(displayPosition)
+//   } else {
+//     alert("Sorry, location is not available. Stay sober, friends.")
+//   }
+
+//   // getBeer();
+// }
+
+// function displayPosition(position) {
+//   const status = document.querySelector('#status');
+//   const mapLink = document.querySelector('#map-link');
+//   const mapImage = document.querySelector('#map-image');
+//   const lat = position.coords.latitude;
+//   const lng = position.coords.longitude;
+//   let loc = lat + ", " + lng;
+//   mapLink.href = '';
+//   mapLink.textContent = '';
+
+//   myLocation.innerHTML = "Latitude: " + lat + ", " + "Longitude: " + lng;
+
+//   mapImage.innerHTML =`
+//     <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=${lng}%2C${lat}%2C${lng}%2C${lat}&amp;layer=mapnik" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=18/${lat}/${lng}">View Larger Map</a></small>
+//     `
+//     // mapLink.innerHTML = `Latitude: ${lat}°, Longitude: ${lng}°`;
+
+//   document.querySelector('#find-user').addEventListener('click', getGeo);
+// }
+
+// function createMap() {
+//   let map = document.getElementById('map'),
+//   target: document.querySelector('map'),
+//   layers: [
+//     new ol.layer.Tile({
+//       source: new ol.source.OSM()
+//     })
+//   ],
+//   view: new ol.View({
+//     center: ol.proj.fromLonLat([lat, lng]),
+//     zoom: 4
+//   })
+// };
+    
+  
+
+
+
+
+//   var map = new ol.Map({
+//     target: document.querySelector('map'),
+//     layers: [
+//       new ol.layer.Tile({
+//         source: new ol.source.OSM()
+//       })
+//     ],
+//     view: new ol.View({
+//       center: ol.proj.fromLonLat([lat, lng]),
+//       zoom: 4
+//     })
+//   });
+// }
+
+// createMap();
