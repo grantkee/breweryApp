a33af8debe4b7a39167b75b0959fe8fb51354aa5
grantkee <grant.d.kee@gmail.com>
2019-08-13 19:12:24 -0500

    added result to map

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function displayPosition(position) {
   myLocation.innerHTML = " Latitude: " + lat + ", " + "Longitude: " + lng;
 }
 
-function createMap() {  
+function createMap(data) {  
   const status = document.querySelector('#status');
   const mapLink = document.querySelector('#map-link');
   const mapImage = document.querySelector('#map-image');
@@ -39,7 +39,7 @@ function createMap() {
     let lng = position.coords.longitude;
       status.textContent = '';
       mapImage.innerHTML =`
-      <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=${lng}%2C${lat}%2C${lng}%2C${lat}&amp;layer=mapnik" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=18/${lat}/${lng}">View Larger Map</a></small>
+      <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=${data.data[0].longitude}%2C${data.data[0].latitude}%2C${data.data[0].longitude}%2C${data.data[0].latitude}&amp;layer=mapnik" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=18/${lat}/${lng}">View Larger Map</a></small>
       `
       mapLink.innerHTML = `Latitude: ${lat} °, Longitude: ${lng} °`;
       getBeer(position, lat, lng);
@@ -60,18 +60,37 @@ document.querySelector('#find-user').addEventListener('click', getBeer);
 
 // createMap();
 
-function getBeer(position, lat, lng){
-  console.log(lat + ", " + lng);
-    fetch(`https://sandbox-api.brewerydb.com/v2/search/geo/point?lat=${lat}&lng=${lng}&key=d2bcebda18ee4340157eff0fc2192936`, {'mode': 'no-cors'})
-    //.then(checkFetch)
-    // console.log("id is working")
-    //.then((result) => result.json())
-    .then((info) => {
-      console.log(lat + lng)
-      let output = `<h2>Results: ${info.totalResults}</h2>`;
-      console.log(info.data);
-    });
+function getBeer(){
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position => {
+      let lat = position.coords.latitude;
+      let lng = position.coords.longitude;
+      const proxy = 'https://cors-anywhere.herokuapp.com/';
+      let api = `${proxy}https://sandbox-api.brewerydb.com/v2/search/geo/point?lat=${lat}&lng=${lng}&key=d2bcebda18ee4340157eff0fc2192936`;
+
+      fetch(api)
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          console.log(data)
+        })
+        //createMap(data)
+    })
   }
+}
+
+  // console.log(lat + ", " + lng);
+  //   fetch(`https://sandbox-api.brewerydb.com/v2/search/geo/point?lat=${lat}&lng=${lng}&key=d2bcebda18ee4340157eff0fc2192936`, {'mode': 'no-cors'})
+  //   //.then(checkFetch)
+  //   // console.log("id is working")
+  //   //.then((result) => result.json())
+  //   .then((info) => {
+  //     console.log(lat + lng)
+  //     let output = `<h2>Results: ${info.totalResults}</h2>`;
+  //     console.log(info.data);
+  //   });
+  // }
         // look at last line of code in dashboard.js -- calls button for getting location
       // let output = `<h2>Beer Near Me!</h2>`
       // data.results.forEach((function(user) {
